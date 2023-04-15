0ca60a900aeb131be65d2c80f0eb4cfa0c26bfa7
srilokhkaruturi <srilokh.karuturi@gmail.com>
2023-04-15 18:31:20 -0500

    Add Name

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 import random
-import names
+import requests
 
 
 class Client:
@@ -9,18 +9,27 @@ class Client:
         self.destination_ip = destination_ip
         self.destination_port = destination_port
         self.s = socket.socket()
+        self.name = Client.get_random_name()
 
     def connect(self):
+        # CONNECT
         self.s.connect((self.destination_ip, self.destination_port))
-        message = self.s.recv(1024).decode()
-        print("[Client]", message)
+
+        # RECEIVE INITIAL SERVER ACK
+        server_ack = self.s.recv(1024).decode()
+        print("[Client %s] %s" % (self.name, server_ack))
+
+        # SEND NAME
+        self.s.send(self.name)
+
+        # RECEIVE NAME ACK
+        name_server_ack = self.s.recv(1024).decode()
+        print("[Client %s] %s" % (self.name, name_server_ack))
+
+        # FINISH
 
     def send(self, msg: str):
         self.s.send(msg.encode())
-
-    @staticmethod
-    def get_random_name():
-        pass
 
     @staticmethod
     def generate():
@@ -42,13 +51,21 @@ class Client:
             else:
                 print(first)
 
+    @staticmethod
+    def get_random_name():
+        response = requests.get("https://randomuser.me/api/")
+        data = dict(response.json())
+        name = data["results"]
+        return data["results"]["name"]["first"]
+
 
 def client():
     client = Client("127.0.0.1", 6000)
     client.connect()
 
-    # while (True):
-    #     expression = input("Input: ")
+    while (True):
+
+        print()
 
 
 client()
