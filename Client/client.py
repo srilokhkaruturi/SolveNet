cdfe728d417dc06f345ee87db8d40c97e40f18d1
srilokhkaruturi <srilokh.karuturi@gmail.com>
2023-04-15 15:51:06 -0500

    Add Client Class

--- Client/client.py
+++ Client/client.py
@@ -1,37 +1,54 @@
 import socket
 import sys
 import random
-
-s = socket.socket()
-host = socket.gethostname()
-port = 6000
-
-s.connect((host, port))
-
-message = s.recv(1024).decode()
+import names
 
 
-def generate():
-    symbol="+-*%^/"
-    for x in range(0, 1000):
+class Client:
+    def __init__(self, destination_ip, destination_port):
+        self.destination_ip = destination_ip
+        self.destination_port = destination_port
+        self.s = socket.socket()
 
-        a = random.randint(0,9)
-        a1 = symbol[random.randint(0,5)]
-        b = random.randint(0,9)
+    def connect(self):
+        self.s.connect((self.destination_ip, self.destination_port))
+        message = self.s.recv(1024).decode()
+        print("[Client]", message)
 
-        first = str(a) + a1 + str(b)
-        if(random.randint(0,1)):
-            b1 = symbol[random.randint(0, 5)]
+    def send(self, msg: str):
+        self.s.send(msg.encode())
+
+    @staticmethod
+    def get_random_name():
+        pass
+
+    @staticmethod
+    def generate():
+        symbol = "+-*%^/"
+        for x in range(0, 1000):
+
             a = random.randint(0, 9)
             a1 = symbol[random.randint(0, 5)]
             b = random.randint(0, 9)
-            first+= b1 + str(a) + a1 + str(b)
-            print(first)
-        else:
-            print(first)
 
-while (True):
-    expression = input("Input: ")
+            first = str(a) + a1 + str(b)
+            if (random.randint(0, 1)):
+                b1 = symbol[random.randint(0, 5)]
+                a = random.randint(0, 9)
+                a1 = symbol[random.randint(0, 5)]
+                b = random.randint(0, 9)
+                first += b1 + str(a) + a1 + str(b)
+                print(first)
+            else:
+                print(first)
 
-    # send message
-    s.send(expression.encode())
+
+def client():
+    client = Client("127.0.0.1", 6000)
+    client.connect()
+
+    # while (True):
+    #     expression = input("Input: ")
+
+
+client()
