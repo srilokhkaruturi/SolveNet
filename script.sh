a5ad89e595feaaec68bab91710ee63a51b7f8266
DOG-BREAD <samueljacobsbuisness@gmail.com>
2023-04-20 19:14:56 -0500

    update script

--- script.sh
+++ script.sh
@@ -3,8 +3,14 @@
 #start server
 x-terminal-emulator -e python ./Server/server.py
 
+#wait for server to start
+sleep 2
+
 #start each client
-for i in $(seq 1 3)
+echo "Enter in your desired number of clients \n"
+read clientCount
+
+for i in $(seq 1 $clientCount)
 do
 	x-terminal-emulator -e python ./Client/client.py
 done
